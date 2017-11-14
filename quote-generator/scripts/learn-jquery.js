e7bc38689285a1a7447b7eff5181fb1905646e08
jonnyrude <jrkrelix_01@hotmail.com>
2017-11-13 20:41:44 -0700

    jquery started

---
+++ quote-generator/scripts/learn-jquery.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    //Basic selectors
+    $('p').css('boarder', '4px solid red');
+
+    //basic animations
+    $('.box:first').hide(500).show(800).delay(300)
+
+    //index filters
+    $('p:first').css('border', '4px solid red');
+
+    // relationship filters
+
+
+    // attribute filters
+    $('p[class="quote-box"]'.css('border', '4px solid red'));
+
+
+});
+ 
