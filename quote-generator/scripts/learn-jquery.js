722ac2be903518b7c07956d18a0069be12e1032a
jonnyrude <jrkrelix_01@hotmail.com>
2017-11-14 21:44:46 -0700

    Basic jQuery elements added

--- quote-generator/scripts/learn-jquery.js
+++ quote-generator/scripts/learn-jquery.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
 
     //Basic selectors
-    $('p').css('boarder', '4px solid red');
+    $('div[class="quote-box"]').css('padding', '100px 100px 100px 100px');
+    $('div[class="quote-box"]').hide(1000).show(800);
 
     //basic animations
-    $('.box:first').hide(500).show(800).delay(300)
+    //$('.box:first').hide(500).show(800).delay(300)
 
     //index filters
-    $('p:first').css('border', '4px solid red');
+    //$('p:first').css('border', '4px solid red');
 
     // relationship filters
 
 
     // attribute filters
-    $('p[class="quote-box"]'.css('border', '4px solid red'));
+    //$('p[class="quote-box"]'.css('border', '4px solid red'));
 
 
 });
- 
