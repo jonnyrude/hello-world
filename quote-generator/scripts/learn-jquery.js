e99fb996daeb4623e2f2ed26d792b326e0d6d2f8
jonnyrude <jrkrelix_01@hotmail.com>
2017-11-16 20:10:02 -0700

    Quote generator functions - almost smoothly.

--- quote-generator/scripts/learn-jquery.js
+++ quote-generator/scripts/learn-jquery.js
@@ -1,39 +1,22 @@
 $(document).ready(function(){
 
      function getQuote(){
-
-
-        $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-            $("#quote").html(a[0].content + "<br />— " + a[0].title)
-
-            cache: false;
+         $.ajax({
+            url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+            success:
+                function(data) {
+                var onceSaid = data.shift();
+                $(".quote").html(onceSaid.content + "<p>— " + onceSaid.title + "</p>");
+                },
+        cache: false,
         });
-
     };
 
     getQuote();
 
-
     $("#quoteButton").on('click', function(){
-        $('#quote').fadeOut(300);
+        $('.quote-box').fadeOut(300);
         getQuote();
-        $('#quote').fadeIn(900);
+        $('.quote-box').fadeIn(900);
     });
-
-
-
-
-    //basic animations
-    //$('.box:first').hide(500).show(800).delay(300)
-
-    //index filters
-    //$('p:first').css('border', '4px solid red');
-
-    // relationship filters
-
-
-    // attribute filters
-    //$('p[class="quote-box"]'.css('border', '4px solid red'));
-
-
 });
