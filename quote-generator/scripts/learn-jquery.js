d7a74849dec6f68555ed4e1bfb510928e4d160d1
Jonathon P. Rude <jrkrelix_01@hotmail.com>
2017-12-08 23:31:13 +0000

    Added background image and small improvements to quote generator

--- quote-generator/scripts/learn-jquery.js
+++ quote-generator/scripts/learn-jquery.js
@@ -6,17 +6,44 @@ $(document).ready(function(){
             success:
                 function(data) {
                 var onceSaid = data.shift();
-                $(".quote").html(onceSaid.content + "<p>— " + onceSaid.title + "</p>");
+                $(".quote").html(onceSaid.content + "<p class='attribution'>— " + onceSaid.title + "</p>");
                 },
         cache: false,
         });
     };
 
+    var backgroundBool = true;
+
+    var backgroundLibrary = [
+        'https://unsplash.com/photos/_zKxPsGOGKg',
+        'https://unsplash.com/photos/T6fDN60bMWY',
+        'https://unsplash.com/photos/MqEUKtGpzqA'
+        ];
+
+    function getBackground() {
+
+        if (backgroundBool === true) {
+
+            backgroundBool = false;
+            $('body').css('background-image', 'url(https://source.unsplash.com/featured/?design');
+        } else {
+            backgroundBool = true;
+            $('body').css('background-image', 'url(https://source.unsplash.com/featured/?nature');
+        }
+    };
+
     getQuote();
+
+    getBackground();
 
     $("#quoteButton").on('click', function(){
         $('.quote-box').fadeOut(300);
         getQuote();
         $('.quote-box').fadeIn(900);
     });
+
+    $('#backgroundButton').on('click', function(){
+        getBackground();
+    });
+
 });
