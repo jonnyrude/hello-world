7747fbbbec60d486960dab7e3ac3ec7ae026f709
jonnyrude <jrkrelix_01@hotmail.com>
2018-02-24 20:19:43 -0700

    Cleaned up Udacity projet

--- udProject/artGrid.js
+++ udProject/artGrid.js
@@ -1,13 +1,24 @@
+var isPainting = false;
+
+$('#grid').on('mousedown', 'td', function(){
+    $( this ).css('background-color', $('#colorSelector').val());
+    isPainting = true;
+});
+
+
+$('#grid').on('mouseup', function(){
+    isPainting = false;
+});
+
+$('#grid').on('mouseover', 'td',function(){
+    if(isPainting){
+        $( this ).css('background-color', $('#colorSelector').val());
+    }
+});
 
 $('document').ready(function() {
-    
     makeGrid([10,10]);
-    setColor();
-    $('#gridWidth').focus();
-
-    $('#colorSelector').on('change', function() {
-       setColor();
-    });
+    $('#gridWidth').focus(); //Cursor to the first grid input
 });
 
 
@@ -17,20 +28,18 @@ function getDimensions() {
     return [width, height];
 }
 
-
 function makeGrid (dimensionsArray) {
     var canvas = $('#grid');
     var w = parseInt(dimensionsArray[0]);
     var h = parseInt(dimensionsArray[1]);
     
     if ((w > 0 && w <= 100) && (h > 0 && h <= 40)) {
-    
         canvas.html(''); //clear canvas of any existing grid
         
         for (var row = 0; row < h; row++) {
-        //add row
             canvas.append('<tr> </tr>')
             var columns = 0;
+            
             while (columns < w) {
                 //add <td> elements to each row
                 canvas.children().last().append('<td></td>')
@@ -42,33 +51,4 @@ function makeGrid (dimensionsArray) {
         else {
             $('.error').css('visibility', 'visible');
         }
-    
-
-
-    function setFlag(tOrF) {
-        sessionStorage.setItem('brushFlag', tOrF);
-    }
-
-    $('td').on('mousedown', function(){
-        var color = sessionStorage.getItem('brushColor');
-        $( this ).css('background-color', color);
-        setFlag(true);
-    });
-
-    $('td').on('mouseenter', function(){
-        if(sessionStorage.getItem('brushFlag')){
-            var color = sessionStorage.getItem('brushColor');
-            $( this ).css('backround-color', color);
-        }
-    });
-
-    $('document').on('mouseup', function(){
-        setFlag(false);
-    });
 }
-
-function setColor() {
-    var color = $('#colorSelector').val();
-    sessionStorage.setItem('brushColor', color);
-}
-
