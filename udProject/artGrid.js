ee6aa6bf8374eaf8cb877c0d5c2c7a6cb9ae101b
JR <jrkrelix_01@hotmail.com>
2018-02-26 20:17:53 -0700

    Commented udacity pixel project for submission

--- udProject/artGrid.js
+++ udProject/artGrid.js
@@ -1,41 +1,67 @@
-let isPainting = false;
+/* UDACITY FRONT END DEVELOPER COURSE
+   Project #3: Pixel Art Maker
+   Submission by Jon Rude (jrkrelix)
+*/
 
-$('#grid').on('mousedown', 'td', function(){ 
+/**************************************
+        SETUP
+****************************************/
+
+let isPainting = false;
+const CANVAS = $('#grid');
+
+$('document').ready(function() {
+    makeGrid();
+});
+
+/**************************************
+        EVENT LISTENERS
+****************************************/
+
+// Create new grid upon form submission
+$('form').on('submit', function() {
+    makeGrid();
+    return false;
+});
+
+// Remaining listeners paint grid cells
+// with click & drag painting
+CANVAS.on('mousedown', 'td', function(){ 
     $( this ).css('background-color', $('#colorSelector').val());
     isPainting = true;
+});
+
+CANVAS.on('mouseover', 'td',function() {
+    if(isPainting){
+        $( this ).css('background-color', $('#colorSelector').val());
+    }
 });
 
 $('body').on('mouseup', function() {
     isPainting = false;
 });
 
-$('#grid').on('mouseover', 'td',function() {
-    if(isPainting){
-        $( this ).css('background-color', $('#colorSelector').val());
-    }
-});
-
-
-$('document').ready(function() {
-    makeGrid();
-});
+/**************************************
+        makeGrid  (Helper function)
+****************************************/
 
 /*
 @description Creates a html table used as painting canvas
 */
 function makeGrid () {
-    const CANVAS = $('#grid');
     const WIDTH = $('#gridWidth').val();
     const HEIGHT = $('#gridHeight').val();
+    
+    //clear canvas of any existing grid
+    CANVAS.html(''); 
 
-    CANVAS.html(''); //clear canvas of any existing grid
-
+    // for loop to create table rows
     for (let row = 0; row < HEIGHT; row++) {
         CANVAS.append('<tr> </tr>');
         let columns = 0;
 
+        // while loop to create table cells 
         while (columns < WIDTH) {
-            //add <td> elements to each row
             CANVAS.children().last().append('<td></td>');
             columns++;
         }
